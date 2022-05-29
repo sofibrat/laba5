92ae88244ac8214b96567b802c8bf8d9869180bb
Sofia Bratkovskaya <bratkovskaya.sofiya@mail.ru>
2022-05-29 20:11:35 +0300

    'calculator'

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                double a;
+                double b;
+                double total;
+                char oper;
+
+                Console.WriteLine("Введите первое число:");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите оператор:");
+                oper = Convert.ToChar(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                if (oper == '+')
+                {
+                    total = a + b;
+                    Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+                }
+
+                else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+                }
+
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+                }
+
+                else if (oper == '/')
+                {
+                    total = a / b;
+                    Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестный оператор.");
+                }
+        }
+    }
+}
+
+
